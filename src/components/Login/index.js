e623d6cc6c3485bf20876f7de4d29ff10899e260
yp.drait@gmail.com <yp.drait@gmail.com>
2018-06-11 00:39:14 +1000

    Login Page

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,26 +1,73 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+
 import { USER_NAME_CHANGE, PASSWORD_CHANGE} from './constants';
 import { onInputChange, applicationLogin } from './actions';
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  wrapper: {
+    minWidth: 600,
+  },
+  paper: {
+    margin: theme.spacing.unit,
+    padding: theme.spacing.unit * 2,
+    minWidth: 400,
+    minHeight: 200,
+    marginTop: '20%'
+  }
+});
 
 
 class Login extends Component{
   render(){
   console.log(this.props);
     return (
-
-      <div>
-        <input
-          type="text"
-          onChange={this.props.onChange(USER_NAME_CHANGE)}
+      <Grid
+      container
+      justify="center"
+      //className={this.props.classes.wrapper}
+      >
+      <Paper className={this.props.classes.paper} >
+      <Grid
+        container
+        className={this.props.classes.root}
+        spacing={16}
+        direction="column"
+        justify="center"
+        alignItems="center"
+      >
+        <TextField
+        id="UserName"
+        label="User Name"
+        onChange={this.props.onChange(USER_NAME_CHANGE)}
+        margin="normal"
         />
-        <input
-          type="password"
+        <TextField
+          id="Password"
+          label="Password"
           onChange={this.props.onChange(PASSWORD_CHANGE)}
-        />
-        <button onClick={this.props.onLogin(this.props.USER_NAME, this.props.PASSWORD)}>Login</button>
-      </div>
+          margin="normal"
+          type="password"
+          />
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={this.props.onLogin(this.props.USER_NAME, this.props.PASSWORD)}
+          >
+          Login
+          </Button>
+      </Grid>
+      </Paper>
+      </Grid>
     );
   }
 }
@@ -45,4 +92,4 @@ const mapDispatchToProps = dispatch => {
   }
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Login);
+export default connect(mapStateToProps, mapDispatchToProps)(withStyles(styles)(Login));
