012a723c61686c024bd0b1463bb079642c076eb5
Yajnesh Padiyar <yp.drait@gmail.com>
2018-06-10 17:36:20 +1000

    Reducer Merge

--- src/store.js
+++ src/store.js
@@ -3,7 +3,7 @@ import { createLogger } from 'redux-logger'
 import createSagaMiddleware from 'redux-saga';
 import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 //import { promiseMiddleware, localStorageMiddleware } from './middleware';
-import reducer from './reducer';
+import reducer from './reducers';
 
 import { routerMiddleware } from 'react-router-redux'
 import createHistory from 'history/createBrowserHistory';
